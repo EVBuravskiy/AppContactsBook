2395b29a356cc1ea80893e37d08544fda5f56a5d
EBuravskiy <elric0sphinx@gmail.com>
2025-07-28 18:07:37 +0500

    Added main viewmodel

---
+++ AppContactBook/AppContactBook/Utilities/AppViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppContactBook.Utilities
+{
+    public class AppViewModel : ObservableObject
+    {
+        private object _currentView;
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { OnPropertyChanged(ref _currentView, value); }
+        }
+    }
+}
---
+++ AppContactBook/AppContactBook/Utilities/RelayCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace AppContactBook.Utilities
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action _execute = null;
+        private readonly Func<object, bool> _canExecute = null;
+
+        public RelayCommand(Action execute, Func<object, bool> canExecute = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute ?? (_ => true);
+        }
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public bool CanExecute(object? parameter) => _canExecute(parameter);
+
+        public void Execute(object? parameter) => _execute();
+    }
+}
